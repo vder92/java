dd43db9565d55b3b4c7981d8984745d1ddb4cb5a
vder92 <124683353+vder92@users.noreply.github.com>
2023-03-06 10:28:53 +0700

    Преобразовал тип функции в boolean

--- src/Loop_for.java
+++ src/Loop_for.java
@@ -2,12 +2,13 @@ public class Loop_for {
     public static void main(String[] args) {
 
         for (int i = 100; i >= 1; i--) {
-            if (evenCheck(i) == 0) {
+            if (evenCheck(i)) {
                 System.out.print(i + " ");
             }
         }
     }
-    public static int evenCheck(int i) {
-        return i % 4;
+
+    public static boolean evenCheck(int i) {
+        return i % 4 == 0;
     }
 }

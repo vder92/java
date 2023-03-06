d2f7e00758306b63b7de317333c88a84135e593e
vder92 <124683353+vder92@users.noreply.github.com>
2023-03-06 10:18:20 +0700

    Переимновал переменные a, b, d

--- src/Average.java
+++ src/Average.java
@@ -1,27 +1,25 @@
-import java.util.Scanner;
-
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите начало диапазона ");
-        int a = scanner.nextInt();
+        int firstNumber = scanner.nextInt();
         System.out.print("Введите конец диапазона ");
-        int b = scanner.nextInt();
+        int lastNumber = scanner.nextInt();
         int amountAll = 0;
         int amountEven = 0;
-        int d = 0;
+        int count = 0;
 
-        for (int i = a; i <= b; i++) {
+        for (int i = firstNumber; i <= lastNumber; i++) {
             //сумма всех чисел диапазона
             amountAll = amountAll + i;
             //сумма четных чисел диапазона
             if (i % 2 == 0) {
                 amountEven = amountEven + i;
-                d++;
+                count++;
             }
         }
-        double averageAll = (double) amountAll / (b - a + 1);
-        double averageEven = (double) amountEven / d;
+        double averageAll = (double) amountAll / (lastNumber - firstNumber + 1);
+        double averageEven = (double) amountEven / count;
         System.out.println("Среднее арифметическое чисел дипазаона = " + averageAll);
         System.out.println("Среднее арифметическое четных чисел диапазона = " + averageEven);
     }

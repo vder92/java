389164058fede0d355beec34129079a7437bb4d0
vder92 <124683353+vder92@users.noreply.github.com>
2023-03-11 15:37:12 +0700

    name changed

--- src/MainPerson.java
+++ src/MainPerson.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main_Person {
+public class MainPerson {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите имя ");
